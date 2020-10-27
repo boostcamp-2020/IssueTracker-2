f4f83d4ce5dc967410a5ca21315800eb46f9da30
fElix-MR <smr3102@naver.com>
2020-10-27 18:44:58 +0900

    [feat] issue dummy data 추가

---
+++ iOS/IssueTrackerApp/IssueTrackerApp/IssueHeader.swift
@@ -0,0 +1,12 @@
+//
+//  IssueHeader.swift
+//  IssueTrackerApp
+//
+//  Created by 서명렬 on 2020/10/27.
+//
+
+import UIKit
+
+class IssueHeader: UICollectionReusableView {
+        
+}
--- iOS/IssueTrackerApp/IssueTrackerApp/IssueList.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/IssueList.swift
@@ -9,4 +9,15 @@ import Foundation
 
 struct IssueList: Hashable {
   var issues: [Issue]
+  
+  static let dummyIssues = [Issue(title: "레이블 목록보기 구현", description: "레이블 전체 목록을 볼 수 있어야 한다 2줄까지 보입니다."),
+                            Issue(title: "마일스톤 목록 보기 구현", description: "마일스톤 목록 보기 구현"),
+                            Issue(title: "레이블 목록보기 구현1", description: "레이블 전체 목록을 볼 수 있어야 한다 2줄까지 보입니다."),
+                            Issue(title: "마일스톤 목록 보기 구현1", description: "마일스톤 목록 보기 구현"),
+                            Issue(title: "레이블 목록보기 구현2", description: "레이블 전체 목록을 볼 수 있어야 한다 2줄까지 보입니다."),
+                            Issue(title: "마일스톤 목록 보기 구현2", description: "마일스톤 목록 보기 구현"),
+                            Issue(title: "레이블 목록보기 구현3", description: "레이블 전체 목록을 볼 수 있어야 한다 2줄까지 보입니다."),
+                            Issue(title: "마일스톤 목록 보기 구현3", description: "마일스톤 목록 보기 구현"),
+                            Issue(title: "레이블 목록보기 구현4", description: "레이블 전체 목록을 볼 수 있어야 한다 2줄까지 보입니다."),
+                            Issue(title: "마일스톤 목록 보기 구현4", description: "마일스톤 목록 보기 구현")]
 }
