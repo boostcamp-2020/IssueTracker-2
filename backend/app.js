efc90430b6642189fcb2c250704b47c0d3c40216
zlrlo <easy10612@gmail.com>
2020-10-27 18:13:55 +0900

    [feat] oauth 기능 추가
    - passport-github 적용

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const passport = require('passport');
+
+var GitHubStrategy = require('passport-github').Strategy;
+
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +20,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+passport.use(new GitHubStrategy({
+    clientID: "d0c795748a8b44c86b63",
+    clientSecret: "e0106b8d76a9bc2d3b677ac56740ca58531ef9a2",
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      //db 설정
+    /*User.findOrCreate({ githubId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });*/
+  }
+));
+
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home
+
+    res.redirect('/');
+  });
 
 module.exports = app;
