25625f531abb98da305ddb0d3885c3e078b1a5fc
msmk530 <msmk530@naver.com>
2020-10-27 14:00:07 +0900

    [chore] styled component 설정
    - babel 플러그인 설정
    
    - styled-component package 추가

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: blue;
+`;
 
 function App() {
-  return <h1>Hello World!!!!!!!@@</h1>;
+  return <Title>Hello World!!!!!!!@@</Title>;
 }
 
 export default App;
