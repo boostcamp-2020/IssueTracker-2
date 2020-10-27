36fb56438dbbd7378dfb62a909d8c717c38de59f
fElix-MR <smr3102@naver.com>
2020-10-27 18:26:46 +0900

    [feat] IssueCollectionView 구현
    - DiffableDataSource / Snapshot을 활용한 collection view 구현
    - 임시 데이터로 cell 구현

--- iOS/IssueTrackerApp/IssueTrackerApp/IssueCell.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/IssueCell.swift
@@ -8,5 +8,27 @@
 import UIKit
 
 class IssueCell: UICollectionViewCell {
-    
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: UILabel!
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    configure()
+  }
+  
+  private func configure() {
+    layer.borderWidth = 0.2
+    layer.borderColor = UIColor.lightGray.cgColor
+  }
+  
+  func updateCell(withTitle title: String, description: String) {
+    titleLabel.text = title
+    descriptionLabel.text = description
+  }
 }
--- iOS/IssueTrackerApp/IssueTrackerApp/IssueViewController.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/IssueViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class IssueViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
-
-
+enum Section {
+  case main
 }
 
+class IssueViewController: UIViewController {
+  
+  typealias DataSource = UICollectionViewDiffableDataSource<Section, Issue>
+  typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Issue>
+  
+  private lazy var dataSource = makeDataSource()
+  
+  @IBOutlet weak var issueCollectionView: UICollectionView!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureNavigationBar()
+    applySnapshot(animatingDifferences: false)
+    issueCollectionView.delegate = self
+    
+  }
+  
+  private func configureNavigationBar() {
+    navigationController?.navigationBar.prefersLargeTitles = true
+  }
+  
+  private func makeDataSource() -> DataSource {
+    let dataSource = DataSource(collectionView: issueCollectionView) { (collectionView, indexPath, issue) -> UICollectionViewCell? in
+      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "IssueCell", for: indexPath) as? IssueCell else { return UICollectionViewCell() }
+      
+      cell.updateCell(withTitle: issue.title,
+                      description: issue.description)
+      
+      return cell
+    }
+    return dataSource
+  }
+  
+  private func applySnapshot(animatingDifferences: Bool = true) {
+    var snapshot = Snapshot()
+    snapshot.appendSections([.main])
+    let issues = [Issue(title: "레이블 목록보기 구현", description: "레이블 전체 목록을 볼 수 있어야 한다 2줄까지 보입니다."),
+                  Issue(title: "마일스톤 목록 보기 구현", description: "마일스톤 목록 보기 구현")]
+    let issueList = IssueList(issues: issues)
+    snapshot.appendItems(issueList.issues)
+    dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+  }
+}
+
+extension IssueViewController: UICollectionViewDelegate {
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    guard let issue = dataSource.itemIdentifier(for: indexPath) else {
+      return
+    }
+    
+    // TODO:- issue 사용
+  }
+}
+
+extension IssueViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: view.bounds.width, height: view.bounds.height / 10)
+  }
+}
