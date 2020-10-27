72b43a0aedaf822c0467a2bd64f49e5350fb7e68
songjucho <pinetree134@ajou.ac.kr>
2020-10-27 15:19:18 +0900

    [feat] 아이디 유효성 검사 구현
    - LoginViewModel 구현 및 바인딩
    - 유효성 검사에 따른 idTextField borderColor 변경

--- iOS/IssueTrackerApp/IssueTrackerApp/LoginViewController.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/LoginViewController.swift
@@ -9,13 +9,37 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    var loginValidator: LoginValidatable?
+    var loginViewModel: LoginViewModelBinding?
     
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginViewModel = LoginViewModel(loginValidator: LoginValidator())
+        idTextField.addTarget(self, action: #selector(editIdTextField), for: .editingChanged)
+        binding()
+    }
+    
+    private func binding() {
+        bindIdTextField()
+        bindPwTextField()
+    }
+    
+    private func bindIdTextField() {
+        loginViewModel?.bindId { [weak self] isValid in
+            DispatchQueue.main.async {
+                self?.idTextField.layer.borderColor = isValid ? UIColor.green.cgColor : UIColor.red.cgColor
+            }
+        }
+    }
+    
+    private func bindPwTextField() {
+    }
+    
+    @objc private func editIdTextField() {
+        guard let id = idTextField.text else { return }
+        loginViewModel?.isValid(id: id)
     }
 }
 
--- iOS/IssueTrackerApp/IssueTrackerApp/LoginViewModel.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/LoginViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+protocol LoginViewModelBinding {
+    func bindId(withHandler handler: @escaping (Bool) -> ())
+    func bindPw(withHandler handler: @escaping () -> ())
+    func isValid(id: String)
+}
+
+class LoginViewModel: LoginViewModelBinding {
+    var loginValidator: LoginValidatable
+    
+    var idHandler: ((Bool) -> ())?
+    var pwHandler: (() -> ())?
+    
+    init(loginValidator: LoginValidatable) {
+        self.loginValidator = loginValidator
+    }
+    
+    func bindId(withHandler handler: @escaping (Bool) -> ()) {
+        idHandler = handler
+    }
+    
+    func bindPw(withHandler handler: @escaping () -> ()) {
+        pwHandler = handler
+    }
+    
+    func isValid(id: String) {
+        idHandler?(loginValidator.isValid(id: id))
+    }
+}
