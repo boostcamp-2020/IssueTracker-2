29fdaaecce9f6f97d37ef88078062120c17c63fd
zlrlo <easy10612@gmail.com>
2020-11-01 18:58:14 +0900

    [chore] db 설정
    - host, user, database, password 정보 설정

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,6 +2,13 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// GitHub OAuth
 exports.clientID = process.env.CLIENT_ID;
 exports.clientSecret = process.env.CLIENT_SECRET;
 exports.clinetURL = process.env.CLIENT_URL;
+
+// Database
+exports.host = process.env.DB_HOST;
+exports.user = process.env.DB_USER;
+exports.database = process.env.DB_NAME;
+exports.password = process.env.DB_PASS;
