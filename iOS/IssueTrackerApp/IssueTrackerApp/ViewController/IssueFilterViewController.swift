0545a0a4c8e0b862559d817289874da7440a6a57
songjucho <pinetree134@ajou.ac.kr>
2020-11-03 18:47:57 +0900

    [feat] Issue Filter TableView 추가
    - diffableDataSource 활용
    - header, section 구현 중

--- iOS/IssueTrackerApp/IssueTrackerApp/ViewController/IssueFilterViewController.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/ViewController/IssueFilterViewController.swift
@@ -7,9 +7,26 @@
 
 import UIKit
 
+enum FilterSection {
+  case main
+}
+
+enum FilterItem: String, CaseIterable {
+  case open = "열린 이슈들"
+  case your = "내가 작성한 이슈들"
+  case assigned = "나한테 할당된 이슈들"
+  case dat = "내가 댓글을 남긴 이슈들"
+  case closed = "닫힌 이슈들"
+}
+
 class IssueFilterViewController: UIViewController {
   
+  typealias DataSource = UITableViewDiffableDataSource<FilterSection, String>
+  typealias Snapshot = NSDiffableDataSourceSnapshot<FilterSection, String>
   
+  lazy var dataSource = makeDataSource()
+  
+  @IBOutlet weak var filterTableView: UITableView!
   @IBAction func cancelButtonTouched(_ sender: UIBarButtonItem) {
     dismiss(animated: true, completion: nil)
   }
@@ -20,6 +37,45 @@ class IssueFilterViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    filterTableView.delegate = self
+    applySnapshot()
   }
   
+  private func makeDataSource() -> DataSource {
+    let dataSource = DataSource(tableView: filterTableView) { (tableView, indexPath, item) -> UITableViewCell? in
+      guard let cell = tableView.dequeueReusableCell(withIdentifier: "IssueFilterCell", for: indexPath) as? FilterIssueCell else { return UITableViewCell() }
+      cell.updateCell(withText: item)
+      return cell
+    }
+    return dataSource
+  }
+  
+  private func applySnapshot(animatingDifferences: Bool = true) {
+    var snapshot = Snapshot()
+    snapshot.appendSections([.main])
+    let items = FilterItem.allCases.map { $0.rawValue }
+    snapshot.appendItems(items)
+    dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+  }
+  
+}
+
+extension IssueFilterViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+      return 30
+  }
+  
+  func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: IssueFilterHeader.reuseIdentifier) as? IssueFilterHeader else { return nil }
+    header.updateHeader(withText: "다음 중에 조건을 고르세요")
+    return header
+  }
+  
+  func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+      return CGFloat.leastNormalMagnitude
+  }
+  
+  func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+      return nil
+  }
 }
