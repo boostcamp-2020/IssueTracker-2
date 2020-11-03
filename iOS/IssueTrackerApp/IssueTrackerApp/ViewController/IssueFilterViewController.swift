4eb20ca12721c1cb1bcd75f21e64e218ad9d76ad
songjucho <pinetree134@ajou.ac.kr>
2020-11-03 17:19:22 +0900

    [feat] filter 화면 Navigation Bar 구현
    - cancel 버튼 구현

--- iOS/IssueTrackerApp/IssueTrackerApp/ViewController/IssueFilterViewController.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/ViewController/IssueFilterViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class IssueFilterViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
+  
+  @IBAction func cancelButtonTouched(_ sender: UIBarButtonItem) {
+    dismiss(animated: true, completion: nil)
+  }
+  
+  @IBAction func doneButtonTouched(_ sender: UIBarButtonItem) {
+    dismiss(animated: true, completion: nil)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
 }
