a4b7a002086cc8c82ab3520a8b748785cf9afb26
msmk530 <msmk530@naver.com>
2020-11-04 22:26:00 +0900

    [feat] Due date input 마크업

--- frontend/src/components/CreateMilestone/inputDueDate.js
+++ frontend/src/components/CreateMilestone/inputDueDate.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-
-export default function InputTitle(props) {
+export default function InputDueDate(props) {
   return (
     <Wrapper>
-      <Title>Title</Title>
-      <TitleInput type="text" placeholder="Title" />
+      <DueDate>Due Date(optional)</DueDate>
+      <DueDateInput type="date" />
     </Wrapper>
   );
 }
@@ -14,16 +13,16 @@ const Wrapper = styled.div`
   padding: 1em 0;
 `;
 
-const Title = styled.div`
+const DueDate = styled.div`
   margin-bottom: 0.5em;
   font-weight: bold;
 `;
-const TitleInput = styled.input`
+const DueDateInput = styled.input`
   width: 40%;
   height: 3em;
   padding: 0 1em;
   border: 1px solid #e1e4e8;
-  font-size: 0.8em;
+  font-size: 1em;
   border-radius: 5px;
   background-color: #fafbfc;
 `;
--- frontend/src/components/CreateMilestone/inputForm.js
+++ frontend/src/components/CreateMilestone/inputForm.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import InputTitle from './inputTitle';
+import InputDueDate from './inputDueDate';
 
 export default function InputForm(props) {
   return (
     <Wrapper>
       <InputTitle />
+      <InputDueDate />
     </Wrapper>
   );
 }
