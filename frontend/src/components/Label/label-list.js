5bf48aa860aca126743a9fe153e66a5630af5051
WooYoo <puba5@naver.com>
2020-11-04 15:46:30 +0900

    [feat] label tag 마크업
    label tag 마크업

--- frontend/src/components/Label/label-list.js
+++ frontend/src/components/Label/label-list.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const LABEL_LIST_INFO = [
+  { name: 'bug', description: '', textColor: 'white', backgroundColor: 'red' },
+  { name: 'bug', description: '', textColor: 'white', backgroundColor: 'red' },
+  { name: 'bug', description: '', textColor: 'white', backgroundColor: 'red' },
+];
+
 export default function LabelList() {
   return (
     <Wrapper>
       <ContentHeader>label</ContentHeader>
-      <LabelDescriptionList></LabelDescriptionList>
+      <LabelDescriptionList>
+        {LABEL_LIST_INFO.map(labelInfo => {
+          return (
+            <LabelDescription>
+              <LabelTag
+                backgroundColor={labelInfo.backgroundColor}
+                textColor={labelInfo.textColor}
+              >
+                {labelInfo.name}
+              </LabelTag>
+            </LabelDescription>
+          );
+        })}
+      </LabelDescriptionList>
     </Wrapper>
   );
 }
@@ -21,4 +40,14 @@ const ContentHeader = styled.div`
 `;
 const LabelDescriptionList = styled.div``;
 
-const LabelDescription = styled.div``;
+const LabelDescription = styled.div`
+  padding: 20px 16px;
+  border-top: 1px solid #dddddd;
+`;
+
+const LabelTag = styled.div`
+  padding: 0 5px;
+  background: ${props => props.backgroundColor};
+  color: ${props => props.textColor};
+  width: fit-content;
+`;
