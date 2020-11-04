889de53107e4cded89f87302a5667c8085c2400f
msmk530 <msmk530@naver.com>
2020-11-04 23:10:28 +0900

    [feat] milestone edit페이지 nav button 추가

--- frontend/src/pages/EditMilestone/editMilestone.js
+++ frontend/src/pages/EditMilestone/editMilestone.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../components/CreateMilestone/header';
-
+import NavButton from '../../components/EditMilestone/navButton';
 import InputForm from '../../components/CreateMilestone/inputForm';
 import Buttons from '../../components/EditMilestone/buttons';
 import Footer from '../../components/CreateMilestone/footer';
@@ -9,6 +9,7 @@ const EditMilestone = props => {
   return (
     <>
       <Header />
+      <NavButton />
       <InputForm />
       <Buttons />
       <Footer />
