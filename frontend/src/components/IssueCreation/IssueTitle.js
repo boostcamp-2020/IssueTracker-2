eec626567cd9c983aa75b8680324670111d594fd
zlrlo <easy10612@gmail.com>
2020-11-04 13:23:33 +0900

    [feat] issue 생성 화면 title 마크업
    - focus 효과 적용

---
+++ frontend/src/components/IssueCreation/IssueTitle.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+export default () => {
+  return (
+    <Title>
+      <GlowingBorder placeholder="Title" type="text" />
+    </Title>
+  );
+};
+
+const Title = styled.div`
+  height: 50px;
+`;
+
+const GlowingBorder = styled.input`
+  width: 100%;
+  padding: 8px 12px;
+  box-sizing: border-box;
+  border: 1px solid #e1e4e8;
+  border-radius: 6px;
+  outline: none;
+  font-size: 16px;
+  background-color: #f6f8fa;
+  &:focus {
+    background-color: #fff;
+    border: 1px solid #9ecaed;
+    box-shadow: 0 0 10px #9ecaed;
+    -webkit-transition: 0.5s;
+    transition: 0.5s;
+  }
+`;
