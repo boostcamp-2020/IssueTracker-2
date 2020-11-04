b12c52c68465c960aa44bc3087d76d8f4ca1bd8e
WooYoo <puba5@naver.com>
2020-11-04 10:18:05 +0900

    [feat] : Label Header 컴포넌트 생성
    레이블 헤더 컴포넌트 생성 및 마크업

--- frontend/src/pages/Label/label.js
+++ frontend/src/pages/Label/label.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Header from '../../components/Label/header';
 
 export default function Label() {
-  return <div>레이블 페이지입니다.</div>;
+  return (
+    <div>
+      <Header />
+      레이블 페이지입니다.
+    </div>
+  );
 }
