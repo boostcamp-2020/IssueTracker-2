30113138d391aea292e905a2c6683a88e1f0e251
fElix-MR <smr3102@naver.com>
2020-11-05 21:35:20 +0900

    [fix] NewIssueButton 수정
    - NewIssueButton에 shadow 관련 이슈 수정

--- iOS/IssueTrackerApp/IssueTrackerApp/Model/IssueList.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/Model/IssueList.swift
@@ -14,5 +14,20 @@ struct IssueList: Hashable {
 //  Issue(id: 0, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm")
   static let b = Issue(id: 3, userSid: 0, issueTitle: "이슈 목록보기 구현", issueAuthor: "fElix-MR", comment: nil, label: nil, milestone: nil, issueStatus: true, assignee: nil)
   
-  static let dummyIssues = [a, b]
+  static let dummyIssues = [
+    Issue(id: 0, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 1, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 2, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 3, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 4, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 5, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 6, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 7, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 8, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 9, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 10, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 11, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 12, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
+    Issue(id: 13, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm")
+  ]
 }
