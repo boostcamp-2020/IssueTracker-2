7018271a4d2c11a9cfc92e2abdb2ea0fd7c5b105
zlrlo <easy10612@gmail.com>
2020-11-05 17:21:10 +0900

    [modify] 변수명 변경
    - commentId -> generatedCommentId
    
    - issueCommentId -> generatedIssueCommentId

--- backend/models/comments.js
+++ backend/models/comments.js
@@ -5,14 +5,14 @@ exports.create = async ({ issueId, writerId, description }) => {
       // 2개 쿼리중 하나 실패하면 둘다 안들어가게 처리 해야함
     const connection = await db.pool.getConnection(async conn => conn);
     let sql1 = 'INSERT INTO comments (writer_id, description) VALUES (?, ?)';
-    const [{ commentId }] = await connection.query(sql1, [writerId, description]);
+    const [{ generatedCommentId }] = await connection.query(sql1, [writerId, description]);
 
     let sql2 = 'INSERT INTO issue_comments (issue_id, comment_id) VALUES (?, ?)';
-    const [{ issueCommentId  }] = await connection.query(sql2, [issueId, commentId]);
+    const [{ generatedIssueCommentId  }] = await connection.query(sql2, [issueId, commentId]);
     
     connection.release();
     
-    return { commentId, issueCommentId };
+    return { generatedCommentId, generatedIssueCommentId };
 
   } catch (err) {
     throw new Error(err);
--- backend/services/comment-service.js
+++ backend/services/comment-service.js
@@ -2,8 +2,8 @@ const users = require('../models/comments');
 
 exports.addCommentService = async req => {
   try {
-    const { commentId, issueCommentId } = await users.create(req.body);
-    return { status: 200, message: 'comment 추가 성공', commentId, issueCommentId };
+    const { generatedCommentId, generatedIssueCommentId } = await users.create(req.body);
+    return { status: 200, message: 'comment 추가 성공', generatedCommentId, generatedIssueCommentId };
   } catch {
     return { status: 400, message: '서비스 실패' };
   }
