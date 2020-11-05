175a3b84005bd5181bf6e9ebb19a60472c6bed4d
WooYoo <puba5@naver.com>
2020-11-05 21:41:33 +0900

    [feat] 형식에 맞게 데이터 수정
    - API 명세에 맞게 변수 이름 수정

--- backend/controllers/label-controller.js
+++ backend/controllers/label-controller.js
@@ -3,5 +3,5 @@ const service = require('../services/label-service');
 exports.readLabel = async (req, res, next) => {
   const { status, message, labelList } = await service.labelService(req);
 
-  res.status(status).json({ message, labelList });
+  res.status(status).json({ message, labels: labelList });
 };
