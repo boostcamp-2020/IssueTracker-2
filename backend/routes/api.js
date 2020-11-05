589297ea15c95fe49801a12202182f81e3998105
zlrlo <easy10612@gmail.com>
2020-11-05 15:25:48 +0900

    [feat] comment 라우터 추가
    - comment 라우터 추가

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRouter = require('./api/user-api');
+const commentRouter = require('./api/comment-api');
 
 router.use('/user', userRouter);
+router.use('/comment', commentRouter);
 
 module.exports = router;
