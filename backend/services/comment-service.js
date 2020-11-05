403a2d5cb2156cac0d40b17315db712807a489cc
zlrlo <easy10612@gmail.com>
2020-11-05 17:15:58 +0900

    [feat] comment delete service 코드 작성
    - controller에 응답코드, 메세지, 삭제된 comment id, issue comment id 전달

--- backend/services/comment-service.js
+++ backend/services/comment-service.js
@@ -1,1 +1,11 @@
 const comments = require('../models/comments');
+
+exports.deleteCommentService = async req => {
+    try {
+      const { commentDeletedId, issueCommentDeletedId } = await comments.delete(req.query);
+  
+      return { status: 200, message: 'comment delete 삭제', commentDeletedId, issueCommentDeletedId };
+    } catch {
+      return { status: 400, message: '서비스 실패' };
+    }
+  };
