416bc54ef609bab575d56727bc9827a7710d0f51
zlrlo <easy10612@gmail.com>
2020-11-05 17:14:12 +0900

    [feat] comment 삭제 controller 작성
    - 클라이언트에 응답코드, 삭제된 comment id, issue comment id 전달

--- backend/controllers/comment-controller.js
+++ backend/controllers/comment-controller.js
@@ -1,1 +1,7 @@
 const service = require('../services/comment-service');
+
+exports.deleteComment = async (req, res, next) => {
+    const { status, message, commentDeletedId, issueCommentDeletedId } = await service.deleteCommentService(req);
+    res.status(status).json({ message, commentDeletedId, issueCommentDeletedId });
+  };
+  
--- backend/routes/api/comment-api.js
+++ backend/routes/api/comment-api.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const controller = require('../../controllers/comment-controller');
 
+router.delete('/', controller.deleteComment);
 
 module.exports = router;
