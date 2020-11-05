79fb04c6012816f91ae5b3f676161f55b90a4fab
zlrlo <easy10612@gmail.com>
2020-11-05 17:12:12 +0900

    [feat] comment delete model 코드 작성
    - comments 테이블에서 comment 삭제
    - issue_comments 테이블에서 comment 삭제

--- backend/models/comments.js
+++ backend/models/comments.js
@@ -1,1 +1,18 @@
 const db = require('../db');
+
+exports.delete = async ({ commentId }) => {
+    try {
+      const connection = await db.pool.getConnection(async conn => conn);
+      let sql1 = 'DELETE FROM comments WHERE id = ?';
+      const [{ commentDeletedId }] = await connection.query(sql1, [commentId]);
+
+      let sql2 = 'DELETE FROM issue_comments WHERE comment_id = ?';
+      const [{ issueCommentDeletedId }] = await connection.query(sql2, [commentId]);
+      connection.release();
+      return { commentDeletedId, issueCommentDeletedId };
+
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+  
