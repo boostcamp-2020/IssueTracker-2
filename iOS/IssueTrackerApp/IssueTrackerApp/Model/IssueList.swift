b479c8159ebb1b6684190d7ed19ce6640aa8d272
fElix-MR <smr3102@naver.com>
2020-11-05 21:37:48 +0900

    [refactor] 필요 없는 코드 삭제

--- iOS/IssueTrackerApp/IssueTrackerApp/Model/IssueList.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/Model/IssueList.swift
@@ -10,10 +10,6 @@ import Foundation
 struct IssueList: Hashable {
   var issues: [Issue]
   
-  static let a = Issue(id: 0, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm")
-//  Issue(id: 0, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm")
-  static let b = Issue(id: 3, userSid: 0, issueTitle: "이슈 목록보기 구현", issueAuthor: "fElix-MR", comment: nil, label: nil, milestone: nil, issueStatus: true, assignee: nil)
-  
   static let dummyIssues = [
     Issue(id: 0, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
     Issue(id: 1, userSid: 0, issueTitle: "레이블 목록보기 구현", issueAuthor: "Godrm"),
