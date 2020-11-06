241be341808a623d34aae973c36a61fba30c4105
msmk530 <msmk530@naver.com>
2020-11-06 21:45:26 +0900

    [feat] Common Component List Form 분리 및 사용

---
+++ frontend/src/components/Common/ListForm.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+import { MilestoneIcon, CheckIcon } from '@primer/octicons-react';
+
+export default function ListForm(props) {
+  const getHeaderContent = type => {
+    if (type === 'milestone') {
+      return (
+        <>
+          <MilestoneIcon size={20} />
+          <Open>
+            <Count>2</Count>
+            <span>Open</span>
+          </Open>
+          <Closed>
+            <CheckIcon size={22} />
+            <Count>0</Count>
+            <span>Closed</span>
+          </Closed>
+        </>
+      );
+    }
+    if (type === 'label') {
+      return (
+        <>
+          <span>13</span>
+          <span>labels</span>
+        </>
+      );
+    }
+  };
+
+  return (
+    <Wrapper>
+      <ListHeader>{getHeaderContent(props.type)}</ListHeader>
+      <List>{props.content}</List>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 90%;
+  height: 25em;
+  margin: auto;
+`;
+
+const ListHeader = styled.div`
+  width: 100%;
+  padding: 0.5em 1.5em;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 5px 5px 0 0;
+  background-color: rgba(0, 0, 0, 0.05);
+`;
+
+const Open = styled.span``;
+const Count = styled.span`
+  margin-left: 0.3em;
+  margin-right: 0.3em;
+`;
+const Closed = styled.span`
+  margin-left: 1em;
+`;
+
+const List = styled.div`
+  width: 100%;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+`;
