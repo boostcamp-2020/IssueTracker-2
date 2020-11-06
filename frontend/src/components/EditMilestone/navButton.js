754c58dd16dc7d07be0cb7ae3c8797f370c5415c
msmk530 <msmk530@naver.com>
2020-11-06 21:10:29 +0900

    [feat] Commont Component Navigation Button 분리 및 사용

--- frontend/src/components/EditMilestone/navButton.js
+++ frontend/src/components/EditMilestone/navButton.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import { TagIcon, MilestoneIcon } from '@primer/octicons-react';
+import NavigationButton from '../Common/NavButton';
 
 export default function NavButton(props) {
   return (
     <Nav>
-      <LabelButton>
-        <TagIcon size={17} />
-        <Text>Labels</Text>
-      </LabelButton>
-      <MilestoneButton>
-        <MilestoneIcon size={17} />
-        <Text>Milestones</Text>
-      </MilestoneButton>
+      <NavigationButton />
     </Nav>
   );
 }
@@ -23,32 +16,4 @@ const Nav = styled.nav`
   padding: 2em 0;
   position: relative;
   border-bottom: 1px solid rgba(0, 0, 0, 0.3);
-`;
-
-const LabelButton = styled.span`
-  display: inline-block;
-  padding: 0.5em 1.5em;
-  border: 1px solid rgba(0, 0, 0, 0.3);
-  border-right: none;
-  border-radius: 5px 0 0 5px;
-  cursor: pointer;
-  &:hover {
-    background-color: rgba(0, 0, 0, 0.03);
-  }
-`;
-
-const Text = styled.span`
-  margin-left: 5px;
-  font-size: 0.9em;
-`;
-
-const MilestoneButton = styled.span`
-  display: inline-block;
-  padding: 0.5em 1em;
-  border: 1px solid rgba(0, 0, 0, 0.3);
-  border-radius: 0 5px 5px 0;
-  cursor: pointer;
-  color: white;
-  background-color: #0366d6;
-  fill: white;
 `;
--- frontend/src/components/Label/nav-bar.js
+++ frontend/src/components/Label/nav-bar.js
@@ -1,37 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import CreateButton from '../Common/GreenButton';
+import NavigationButton from '../Common/NavButton';
 
 export default function NavBar() {
   return (
     <Wrapper>
-      <OptionButtonsWrapper>
-        <OptionButton borderRight="1px solid #aaaaaa">Label</OptionButton>
-        <OptionButton>Milestones</OptionButton>
-      </OptionButtonsWrapper>
-      <CreateButton content="New label" />
+      <NavigationButton />
+      <ButtonWrapper>
+        <CreateButton content="New label" />
+      </ButtonWrapper>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 50px;
+  width: 100%;
+  position: relative;
 `;
 
-const OptionButtonsWrapper = styled.div`
-  border-radius: 6px;
-  border: 1px solid #aaaaaa;
-`;
-
-const OptionButton = styled.button`
-  background: none;
-  border: none;
-  border-right: ${props => props.borderRight};
-  padding: 5px 16px;
-  font-size: 14px;
-  font-weight: 500;
-  height: 35px;
+const ButtonWrapper = styled.div`
+  position: absolute;
+  right: 0;
 `;
