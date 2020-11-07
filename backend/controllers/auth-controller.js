3e8ca06afa0af04b1d4d24d16d27a0696b77fbfe
zlrlo <easy10612@gmail.com>
2020-11-08 01:33:41 +0900

    [feat] 토큰 발행 코드 구현
    - 로그인 성공시 토큰 쿠키에 넣어준다.

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 exports.loginGitHub = (req, res, next) => {
   passport.authenticate('github', function (err, user, info) {
@@ -30,3 +31,29 @@ exports.loginGitHub = (req, res, next) => {
     res.redirect('http://localhost:8080');
   })(req, res, next);
 };
+
+exports.loginPassport = (req, res, next) => {
+  passport.authenticate('local', function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
+
+    if(!user) {
+      res.status(400).json({ message: info.message });
+    }
+
+    /**
+     * jwt 토큰 발급
+     * jwt.sign(payload, secretOrPrivateKey, [options, callback])
+     */
+    const payload = { id: user.sid, nickname: user.nickname };
+    const secret = 'secret';
+    const options = { expiresIn: '7d', subject: 'userInfo' };
+    const token = jwt.sign(payload, secret, options);
+    res.cookie('jwt', token);
+    res.json({ message: 'logged in successfully', token });
+
+  })(req, res, next);
+};
+
+
