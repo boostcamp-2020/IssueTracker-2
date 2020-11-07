511042e6dee4e82c510e77ae91685242966d44c6
zlrlo <easy10612@gmail.com>
2020-11-08 01:32:48 +0900

    [feat] passport jwt 로그인 기능 구현
    - verifyPassport : 입력 받은 user와 db데이터를 확인 후 로그인 진행
    - 비밀번호 암호화 필요함
    - cookieExtractor : 쿠키에 담긴 쿠기 추출
    - verifyJwt : 토큰에 담긴 user와 db 데이터를 확인 후 로그인 진행

--- backend/passport/index.js
+++ backend/passport/index.js
@@ -1,5 +1,9 @@
 const passport = require('passport');
 const GitHubStrategy = require('passport-github').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+
+const users = require('../models/users');
 const config = require('../config');
 
 const githubConfig = {
@@ -18,6 +22,64 @@ const verifyGitHub = (accessToken, refreshToken, profile, cb) => {
   return cb(null, user);
 };
 
+const verifyPassport = async(username, password, done) => {
+    try {
+      const userInfo = await users.findAll({username});
+      
+      if(!userInfo) {
+        return done(null, false, { message: 'This user does not exist' });
+      }
+
+      /**
+       * 비밀번호 암호화 필요
+       */
+      if(password === userInfo.password)  {
+        return done(null, userInfo);
+      }
+      else {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+
+    } catch(err) {
+      return done(err);
+    } 
+}
+
+var cookieExtractor = (req) => {
+  var token = null;
+  if (req && req.cookies)
+  {
+      token = req.cookies['jwt'];
+  }
+  return token;
+};
+
+const jwtConfig = { 
+  jwtFromRequest: cookieExtractor,
+  secretOrKey: 'secret'
+ };
+
+const verifyJwt = async(jwt_payload, done) => {
+  try {
+    const username = jwt_payload.nickname; 
+    const userInfo = await users.findAll({username});
+
+    console.log(username); 
+    console.log(userInfo); 
+
+    if(!userInfo) {
+      return done(null, false, { message: 'This user does not exist' });
+    }
+
+    return done(null, userInfo);
+
+  } catch(err) {
+    return done(err);
+  } 
+};
+
 module.exports = () => {
   passport.use(new GitHubStrategy(githubConfig, verifyGitHub));
+  passport.use(new LocalStrategy(verifyPassport));
+  passport.use(new  JwtStrategy(jwtConfig, verifyJwt));
 };
