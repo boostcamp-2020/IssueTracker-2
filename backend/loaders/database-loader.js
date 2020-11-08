48a2de460be06de9aa36659c009b1d1b5f1c03a0
WooYoo <puba5@naver.com>
2020-11-08 20:45:14 +0900

    [feat] 모든 테이블 생성
    서버를 시작할 때 모든 테이블 생성

--- backend/loaders/database-loader.js
+++ backend/loaders/database-loader.js
@@ -146,11 +146,131 @@ const createMilestoneTable = async () => {
   }
 };
 
-const initializeTables = () => {
+const createCommentTable = async () => {
+  try {
+    const connection = await db.pool.getConnection(async conn => conn);
+
+    connection
+      .query(SQL_DROP_COMMENT_TABLE)
+      .then(() => connection.query(SQL_CREATE_COMMNET_TABLE))
+      .then(() => connection.release());
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const createCommentEmojiTable = async () => {
+  try {
+    const connection = await db.pool.getConnection(async conn => conn);
+
+    connection
+      .query(SQL_DROP_COMMENT_EMOJI_TABLE)
+      .then(() => connection.query(SQL_CREATE_COMMNET_EMOJI_TABLE))
+      .then(() => connection.release());
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const createIssueLabelTable = async () => {
+  try {
+    const connection = await db.pool.getConnection(async conn => conn);
+
+    connection
+      .query(SQL_DROP_ISSUE_LABEL_TABLE)
+      .then(() => connection.query(SQL_CREATE_ISSUE_LABEL_TABLE))
+      .then(() => connection.release());
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const createIssueCommentTable = async () => {
+  try {
+    const connection = await db.pool.getConnection(async conn => conn);
+
+    connection
+      .query(SQL_DROP_ISSUE_COMMENT_TABLE)
+      .then(() => connection.query(SQL_CREATE_ISSUE_COMMENT_TABLE))
+      .then(() => connection.release());
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const createIssueTable = async () => {
+  try {
+    const connection = await db.pool.getConnection(async conn => conn);
+
+    connection
+      .query(SQL_DROP_ISSUE_TABLE)
+      .then(() => connection.query(SQL_CREATE_ISSUE_TABLE))
+      .then(() => connection.release());
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const createIssueAssigneeTable = async () => {
+  try {
+    const connection = await db.pool.getConnection(async conn => conn);
+
+    connection
+      .query(SQL_DROP_ISSUE_ASSIGNEE_TABLE)
+      .then(() => connection.query(SQL_CREATE_ISSUE_ASSIGNEE_TABLE))
+      .then(() => connection.release());
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const createTableRelation = async () => {
+  try {
+    const connection = await db.pool.getConnection(async conn => conn);
+
+    connection
+      .query(
+        `
+        ALTER TABLE issues ADD FOREIGN KEY (user_sid) REFERENCES users (sid); ​
+        ALTER TABLE issues ADD FOREIGN KEY (milestone_id) REFERENCES milestones (id);​
+        ALTER TABLE issue_assignees ADD FOREIGN KEY (assignee_id) REFERENCES users (sid);
+        ALTER TABLE issue_assignees ADD FOREIGN KEY (issue_id) REFERENCES issues (id);
+        ALTER TABLE issue_comments ADD FOREIGN KEY (issue_id) REFERENCES issues (id);
+        ALTER TABLE issue_comments ADD FOREIGN KEY (comment_id) REFERENCES comments (id);
+        ALTER TABLE issue_labels ADD FOREIGN KEY (label_name) REFERENCES labels (id);
+        ALTER TABLE issue_labels ADD FOREIGN KEY (issue_id) REFERENCES issues (id);
+        ALTER TABLE comments ADD FOREIGN KEY (writer_id) REFERENCES users (sid);
+        ALTER TABLE comment_emojis ADD FOREIGN KEY (commnet_id) REFERENCES comments (id);
+        ALTER TABLE comment_emojis ADD FOREIGN KEY (user_id) REFERENCES users (sid);
+        ALTER TABLE comment_emojis ADD FOREIGN KEY (emoji_id) REFERENCES emojis (id);
+      `,
+      )
+      .then(() => connection.release());
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const initializeTables = async () => {
+  await makeTables();
+  //   makeRelation();
+};
+
+const makeTables = () => {
   createUserTable();
   createEmojiTable();
   createLabelTable();
+  createIssueTable();
   createMilestoneTable();
+  createCommentTable();
+  createCommentEmojiTable();
+  createIssueLabelTable();
+  createIssueCommentTable();
+  createIssueAssigneeTable();
+};
+
+const makeRelation = () => {
+  createTableRelation();
 };
 
 initializeTables();
