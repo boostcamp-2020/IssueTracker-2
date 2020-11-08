cfd2e97b3315917dc884dba9899ce83cc5693e0b
msmk530 <msmk530@naver.com>
2020-11-08 16:49:05 +0900

    [fix] update를 위한 HTTP 메소드 통일

--- backend/routes/api/milestone-api.js
+++ backend/routes/api/milestone-api.js
@@ -3,7 +3,7 @@ const controller = require('../../controllers/milestone-controller');
 
 router.post('/', controller.addMilestone);
 router.get('/all', controller.getMilestones);
-router.patch('/', controller.updateMilestone);
+router.put('/', controller.updateMilestone);
 router.delete('/', controller.deleteMilestone);
 
 module.exports = router;
