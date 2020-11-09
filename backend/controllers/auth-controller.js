5ff6f4de4157909604ff25b5a6f1f90a0dec9043
zlrlo <easy10612@gmail.com>
2020-11-10 00:16:21 +0900

    [modify] login 코드 위치 변경, logout 추가
    - auth -> user
    - logout 쿠키 삭제

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -9,7 +9,7 @@ exports.loginGitHub = (req, res, next) => {
 
     if (req.headers['user-agent'].includes('iPhone')) {
       console.log('iphone here');
-      return res.redirect(`issuetrackerpastel://${user}`);
+      return res.redirect(`issuetrackerpastel://${token}`);
     }
 
     // redirect 주소 수정 필요
@@ -17,43 +17,25 @@ exports.loginGitHub = (req, res, next) => {
       return res.redirect('http://localhost:8080');
     }
 
-    /**
-     * 토큰 생성 부분 넣으면 될 듯
-     * req.logIn(user, function(err) {
-     *     if (err) { return next(err); }
-     *     return res.redirect('/users/' + user.username);
-     * });
-     */
-
-    res.cookie('user', user);
+     /**
+      * jwt 토큰 발급
+      * jwt.sign(payload, secretOrPrivateKey, [options, callback])
+      */
+     const payload = { id: user.sid, nickname: user.nickname };
+     const secret = 'secret';
+     const options = { expiresIn: '7d', subject: 'userInfo' };
+     const token = jwt.sign(payload, secret, options);
 
     // redirect 주소 수정 필요
-    res.redirect('http://localhost:8080');
+    res.redirect('http://localhost:8080/?valid=' + token);
   })(req, res, next);
 };
 
-exports.loginPassport = (req, res, next) => {
-  passport.authenticate('local', function (err, user, info) {
-    if (err) {
-      return next(err);
-    }
-
-    if(!user) {
-      res.status(401).json({ message: info.message });
-    }
-
-    /**
-     * jwt 토큰 발급
-     * jwt.sign(payload, secretOrPrivateKey, [options, callback])
-     */
-    const payload = { id: user.sid, nickname: user.nickname };
-    const secret = 'secret';
-    const options = { expiresIn: '7d', subject: 'userInfo' };
-    const token = jwt.sign(payload, secret, options);
-    res.cookie('jwt', token);
-    res.status(202).json({ message: 'logged in successfully' });
-
-  })(req, res, next);
+exports.logout = (req, res, next) => {
+  res.clearCookie('jwt'); 
+  res.status(202).json({ message: 'logout successfully' });
 };
+
+
 
 
