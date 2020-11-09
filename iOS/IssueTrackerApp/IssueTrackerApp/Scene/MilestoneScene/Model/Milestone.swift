0755d35508a02199cc3fe42a852ce6492022ea06
fElix-MR <smr3102@naver.com>
2020-11-09 22:32:38 +0900

    [feat] MilestoneResponse 모델 추가
    - API response를 위한 모델 추가
    - Milestone 모델에 초기값 설정

--- iOS/IssueTrackerApp/IssueTrackerApp/Scene/MilestoneScene/Model/Milestone.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/Scene/MilestoneScene/Model/Milestone.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 struct Milestone: Codable, Hashable {
-  var id: Int
+  var id: Int = 0
   var milestoneName: String
   var milestoneDescription: String?
   var endDate: String
-  var status: Int
-  var openCount: Int
-  var closeCount: Int
+  var status: Int = 0
+  var openCount: Int = 0
+  var closeCount: Int = 0
 }
