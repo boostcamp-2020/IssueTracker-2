2e6138f5d96350dff58c471c96295b38242cb609
zlrlo <easy10612@gmail.com>
2020-11-09 23:06:56 +0900

    [feat] useFetch 작성, click 핸들러 작성
    - 크롱님 custom hooks 적용
    - click 이벤트 발생시 status 변경

--- frontend/src/components/Milestone/milestoneList.js
+++ frontend/src/components/Milestone/milestoneList.js
@@ -1,22 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import LeftContent from './leftContent';
 import RightContent from './rightContent';
 import ListForm from '../Common/ListForm';
 
+// custom hooks
+// 분리해서 사용하면 좋을 듯
+const useFetch = (callback, url) => {
+  const [loading, setLoading] = useState(false);
+
+  const fetchInitialData = async () => {
+    setLoading(true);
+    const response = await fetch(url, { method: 'GET' });
+    const initialData = await response.json();
+    callback(initialData.milestonesInfo);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchInitialData();
+  }, []);
+
+  return loading;
+};
+
+const List = ({ milestones, loading }) => {
+  let milestoneList = <div>Loading...</div>;
+  if (!loading) {
+    milestoneList = milestones.map(milestone => (
+      <Milestone key={milestone.id}>
+        <LeftContent milestone={milestone} />
+        <RightContent milestone={milestone} />
+      </Milestone>
+    ));
+  }
+
+  return <>{milestoneList}</>;
+};
+
 export default function MilestoneList(props) {
+  const [status, setStatus] = useState(0);
+  const [milestones, setMilestones] = useState({
+    openTotalCount: 0,
+    closeTotalCount: 0,
+    milestoneArray: [],
+  });
+
+  const loading = useFetch(
+    setMilestones,
+    `http://localhost:3000/api/milestone/all?status=${status}`,
+  );
+
+  const clickOpen = e => {
+    console.log('clicked open');
+    if (status) {
+      setStatus(0);
+    }
+  };
+
+  const clickClose = e => {
+    console.log('clicked close');
+    if (!status) {
+      setStatus(1);
+    }
+  };
+
   return (
     <ListForm
       content={
-        <>
-          <Milestone>
-            <LeftContent />
-            <RightContent />
-          </Milestone>
-        </>
+        <List milestones={milestones.milestoneArray} loading={loading} />
       }
       type="milestone"
+      openTotalCount={milestones.openTotalCount}
+      closeTotalCount={milestones.closeTotalCount}
+      clickOpen={clickOpen}
+      clickClose={clickClose}
+      status={status}
     />
   );
 }
