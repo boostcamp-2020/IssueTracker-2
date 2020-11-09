4ba4909e4d52071056ccc0aa45ed1153df53ac5f
zlrlo <easy10612@gmail.com>
2020-11-10 00:18:26 +0900

    [modify] login 라우터 위치 변경
    - auth -> user

--- backend/routes/api/user-api.js
+++ backend/routes/api/user-api.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('../../controllers/user-controller');
 
 router.post('/', controller.addUser);
+router.post('/login', controller.loginPassport);
 
 module.exports = router;
