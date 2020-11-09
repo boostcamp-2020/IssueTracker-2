d63e8ef6fdce637a5f813967f6c13aa39194cfeb
msmk530 <msmk530@naver.com>
2020-11-10 00:49:31 +0900

    [feat] comment form 마크업

--- frontend/src/pages/IssueDetail/issueDetail.js
+++ frontend/src/pages/IssueDetail/issueDetail.js
@@ -7,6 +7,7 @@ import IssueInfo from '../../components/IssueDetail/issueInfo';
 import CommentForm from '../../components/IssueCreation/CommentForm';
 import Sidebar from '../../components/IssueCreation/Sidebar';
 import Profile from '../../components/IssueCreation/Profile';
+import Comments from '../../components/IssueDetail/comment';
 
 const IssueDetail = props => {
   const { id } = useParams();
@@ -15,31 +16,52 @@ const IssueDetail = props => {
     <>
       <Header />
       <IssueInfo issueNumber={id} />
-      <IssueCreationBody>
-        <WriteArea>
-          <Profile />
-          <CommentForm />
-        </WriteArea>
+      <CommentBody>
+        <IssueCommentWrap>
+          <Comment>
+            <Profile />
+            <Comments />
+          </Comment>
+          <Comment>
+            <Profile />
+            <Comments />
+          </Comment>
+          <WriteArea>
+            <Profile />
+            <CommentForm />
+          </WriteArea>
+        </IssueCommentWrap>
         <SelectArea>
           <Sidebar />
           <Sidebar />
           <Sidebar />
         </SelectArea>
-      </IssueCreationBody>
+      </CommentBody>
       <Footer />
     </>
   );
 };
 
-const IssueCreationBody = styled.div`
+const CommentBody = styled.div`
   width: 90%;
   display: flex;
-
   height: 700px;
   margin: auto;
 `;
-
+const IssueCommentWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+`;
+const Comment = styled.div`
+  display: flex;
+  width: 100%;
+  margin-bottom: 2em;
+`;
 const WriteArea = styled.div`
+  margin-top: 2em;
+  border-top: 1px solid rgba(0, 0, 0, 0.3);
+  padding: 2em 0;
   display: flex;
   width: 100%;
 `;
