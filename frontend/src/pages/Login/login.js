3d6b5e52256a36ee3896241736e0eb11fa32bfb4
msmk530 <msmk530@naver.com>
2020-11-11 10:21:31 +0900

    [fix] fetch 및 로그인 변경사항

--- frontend/src/pages/Login/login.js
+++ frontend/src/pages/Login/login.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import UserManagementButtons from '../../components/Login/userManagementButtons';
 import UserInput from '../../components/Login/userInput';
+import { useHistory } from 'react-router-dom';
+import { getFetch } from '../../service/fetch';
 
-const Login = () => {
+const Login = ({ setUserInfo }) => {
   const [userLoginInfo, setUserLoginInfo] = useState({ id: null, pw: null });
+
+  const history = useHistory();
 
   return (
     <Wrapper>
