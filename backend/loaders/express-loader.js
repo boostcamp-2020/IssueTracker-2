23d400c3b98636a7bce7f072fc01b68e6605b507
msmk530 <msmk530@naver.com>
2020-11-11 10:22:47 +0900

    [fix] cors수정 및 user api 수정

--- backend/loaders/express-loader.js
+++ backend/loaders/express-loader.js
@@ -5,7 +5,12 @@ const logger = require('morgan');
 const cors = require('cors');
 
 module.exports = app => {
-  app.use(cors());
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    }),
+  );
   app.use(logger('dev'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
