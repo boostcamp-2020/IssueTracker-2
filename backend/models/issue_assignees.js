5b32b3969063d6bbc129221646a4d7222bb6618a
zlrlo <easy10612@gmail.com>
2020-11-12 18:17:08 +0900

    [feat] assignee, label 배열로 받아서 insert하는 기능 구현
    - issue create api
    - assignee, label, issue create 발생

--- backend/models/issue_assignees.js
+++ backend/models/issue_assignees.js
@@ -1,8 +1,7 @@
 const db = require('../db');
 
-exports.create = async ({ issue_id, assignee_id }) => {
+exports.create = async (connection, { issue_id, assignee_id }) => {
   try {
-    const connection = await db.pool.getConnection(async conn => conn);
     let sql =
       'INSERT INTO issue_assignees (issue_id, assignee_id) VALUES ( ?, ?)';
     const [{ insertId }] = await connection.query(sql, [issue_id, assignee_id]);
