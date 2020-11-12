e4ad333c303f240c1c26f83118ce4c12372243ca
zlrlo <easy10612@gmail.com>
2020-11-12 11:30:00 +0900

    [feat] token check 기능 추가
    - comment, issue, label, milestone 모든 api에 적용

--- backend/routes/api/issue-api.js
+++ backend/routes/api/issue-api.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const controller = require('../../controllers/issue-controller');
+const token = require('../../controllers/token-controller');
 
-router.post('/', controller.createIssue);
-router.get('/all', controller.getAllIssues);
-router.put('/', controller.updateIssue);
-router.delete('/', controller.deleteIssue);
+router.post('/', token.check, controller.createIssue);
+router.get('/all', token.check, controller.getAllIssues);
+router.put('/', token.check, controller.updateIssue);
+router.delete('/', token.check, controller.deleteIssue);
 
 module.exports = router;
